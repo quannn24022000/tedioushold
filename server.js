69041d48a5da51a983cbebd18497d5c4479098e2
quannn181705 <quan.nn181705@sis.hust.edu.vn>
2022-11-09 15:53:18 +0700

    first commit for prosper branch

--- server.js
+++ server.js
@@ -8,48 +8,57 @@ const HTTP_PORT = 8000;
 
 const wsServer = new WebSocket.Server({ port: WS_PORT }, () => console.log(`WS Server is listening at ${WS_PORT}`));
 
-// let connectedClients = [];
-// wsServer.on("connection", (ws, req) => {
-// 	console.log("Connected");
+let connectedClients = [];
 
-// 	ws.on("message", (data) => {
-// 		if (data.indexOf("WEB_CLIENT") !== -1) {
-// 			connectedClients.push(ws);
-// 			console.log("WEB_CLIENT ADDED");
-// 			return;
-// 		}
+//1. Single Camera
+//ws: refer for a single connection on server side
+// wsServer.on('connection', (ws, req)=>{
+//     console.log('Connected');
+//     connectedClients.push(ws);
 
-// 		connectedClients.forEach((ws, i) => {
-// 			if (connectedClients[i] == ws && ws.readyState === ws.OPEN) {
-// 				ws.send(data);
-// 			} else {
-// 				connectedClients.splice(i, 1);
-// 			}
-// 		});
-// 	});
-
-// 	ws.on("error", (error) => {
-// 		console.error("WebSocket error observed: ", error);
-// 	});
+//     ws.on('message', data => {
+//         //forEach callback function return ws[the element of array] & i[index of array]
+//         connectedClients.forEach((ws,i)=>{
+//             if(ws.readyState === ws.OPEN){
+//                 ws.send(data);   //data was received from ESP32 is array of binary (hexa)
+//             }else{
+//                 connectedClients.splice(i ,1);
+//             }
+//         })
+//     });
 // });
 
 
-//ws: refer for a single connection on server side
-let connectedClients = [];
-wsServer.on('connection', (ws, req)=>{
-    console.log('Connected');
-    connectedClients.push(ws);
+//2. Multiple Camera
+wsServer.on("connection", (ws, req) => {
+	console.log("Connected");
 
-    ws.on('message', data => {
-        //forEach callback function return ws[the element of array] & i[index of array]
-        connectedClients.forEach((ws,i)=>{
-            if(ws.readyState === ws.OPEN){
-                ws.send(data);
-            }else{
-                connectedClients.splice(i ,1);
-            }
-        })
-    });
+	ws.on("message", (data) => {
+		if (data.indexOf("WEB_CLIENT") !== -1) {
+			connectedClients.push(ws);
+			console.log("WEB_CLIENT ADDED");
+			return;                         // Khi data chi la su kien connect vao SERVER (WEB_CLIENT) thi return va chi return cho callback cua "onmessage" chu khong phai "connection"
+		}                               
+
+        // Khi message khong phai la "WEB_CLIENT" (hay la connection)
+        // forEach callback function return ws[the element of array] & i[index of array]
+		connectedClients.forEach((ws, i) => {
+            // Chi client nao dang vao web (request 192.168.3.122:8000) thi moi nhan duoc message cua rieng no
+            // Server tao cho moi client 1 "ws" de lien lac va tra du lieu cho client
+            // VD: ws esp32 có ws="abc" liên tục gửi dữ liệu từ sever qua lời gọi ws.send(data). Thực ra là câu lệnh này cũng chỉ dành cho ESP32 gửi data mà thôi chứ WEB client chả gửi data gì vào socket
+			if (connectedClients[i] == ws && ws.readyState === ws.OPEN) {
+                // Bản chất thì mỗi ESP32 Cam gửi dữ liệu lên Server và Server đẩy xuống cho client mà thôi
+                // Server gửi sang dashboard.html. Và dashboard.html phân biệt "data" thông qua ID để hiển thị lên Web cho đúng thẻ src của image (cho đúng khung mà thôi)
+				ws.send(data);          
+			} else {
+				connectedClients.splice(i, 1);  //splice 1 element from array
+			}
+		});
+	});
+
+	ws.on("error", (error) => {
+		console.error("WebSocket error observed: ", error);
+	});
 });
 
 app.use(express.static("."));
